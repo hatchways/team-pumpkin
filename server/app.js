679dc123e18367d52f2b344e1bdf7085e437d07e
TheAllen <allenlee1995@hotmail.com>
2020-10-14 11:48:45 -0600

    Implemented auth middleware, register route, and login route

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const { join } = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const cors = require("cors");
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
+
+/*Register and SignIn*/
+const register = require("./routes/register");
+const signin = require("./routes/signin");
 
 const { json, urlencoded } = express;
 
@@ -13,20 +17,24 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(json());
+app.use(cors());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+/*Register and SignIn*/
+app.use("/api", register);
+app.use("/api", signin);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
---
+++ server/routes/signin.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+//Middleware
+const authentication = require("../middleware/authentication");
+
+const jwt = require("jsonwebtoken");
+
+//Model
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+/*
+    Type: POST route
+    Desc: For User Login. Check authentication/ get JWT
+    Acc: public
+*/
+router.post(
+  "/signin",
+  [
+    check("email", "Please enter email").isEmail(),
+    check("password", "Please enter password").exists(),
+  ],
+  async (req, res) => {
+    //If errors occur
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      return res.status(400).json({ errors: err.array() });
+    }
+
+    //Deconstructing the email and password
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errros: [{ msg: "Invalid Credentials" }] });
+      }
+
+      //Password match
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //Sign JWT
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: 1.577e7 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ sucess: true, token });
+        }
+      );
+    } catch (err) {
+      res.status(500).send("Server error:(");
+    }
+  }
+);
+
+module.exports = router;
