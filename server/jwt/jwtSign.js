d4c1ace9370e0e46f1408945d88b73766a83eae1
TheAllen <allenlee1995@hotmail.com>
2020-10-15 14:14:16 -0600

    fixed jwtSign function and added useCreateIndex in db to suppress warning

--- server/jwt/jwtSign.js
+++ server/jwt/jwtSign.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (payload, secret, expireIn, res, userObject) {
-  jwt.sign(payload, secret, expireIn, (err, token) => {
+  jwt.sign(payload, secret, { expiresIn: expireIn }, (err, token) => {
     if (err) throw err;
     res.json({ success: true, token, userObject });
   });
