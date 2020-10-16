77c3794c97b68f5b83209f63513bb17bd654537c
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-15 21:03:10 -0400

    add delete endpoint

--- server/routes/polls/polls.js
+++ server/routes/polls/polls.js
@@ -80,4 +80,30 @@ router.put('/update', authentication, async (req, res) => {
   }
 });
 
+router.put('/delete', authentication, async (req, res) => {
+  try {
+    const userId = req.user.user.id;
+    const { pollId } = req.body;
+    const userPollsData = await PollsMongoModel.findOne({
+      userId,
+    });
+    if (!!userPollsData) {
+      const response = await PollsMongoModel.update(
+        { userId },
+        {
+          $pull: {
+            polls: { pollId },
+          },
+        },
+      );
+      res.status(200).json(response);
+    }
+  } catch (err) {
+    const error = {
+      msg: err,
+    };
+    res.status(400).json(error);
+  }
+});
+
 module.exports = router;
