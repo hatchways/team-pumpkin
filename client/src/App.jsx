3c08eb17ca31df98c7da3fc99c6f4e85a51b2490
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-16 19:52:22 -0400

    add navbar with context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 import { MuiThemeProvider } from '@material-ui/core';
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Header, LoadingScreen } from './components';
 import { HomeScreen, LoginScreen, SignUpScreen } from './LazyComponents';
 import { theme } from './themes/theme';
+import { GlobalContext } from './utils';
 
-function App() {
+const App = () => {
+  const appContext = useContext(GlobalContext);
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header />
+        {!!appContext.user && appContext.user.success && <Header />}
         <Switch>
           <Suspense fallback={<LoadingScreen />}>
-            {/* <Route exact path='/' component={LandingPage} /> */}
             <Route exact path='/signup' component={SignUpScreen} />
             <Route exact path='/login' component={LoginScreen} />
             <Route exact path='/home' component={HomeScreen} />
@@ -22,6 +24,6 @@ function App() {
       </BrowserRouter>
     </MuiThemeProvider>
   );
-}
+};
 
 export default App;
