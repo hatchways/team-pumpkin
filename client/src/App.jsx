6820e3ee530b775f02e1bfcd4b044b65b9457823
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-16 19:15:06 -0400

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
 import { MuiThemeProvider } from '@material-ui/core';
 import React, { Suspense } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { LoadingScreen } from './components';
+import { Header, LoadingScreen } from './components';
 import { HomeScreen, LoginScreen, SignUpScreen } from './LazyComponents';
-import LandingPage from './pages/Landing';
 import { theme } from './themes/theme';
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Suspense fallback={<LoadingScreen />}>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/signup' component={SignUpScreen} />
-          <Route exact path='/login' component={LoginScreen} />
-          <Route exact path='/home' component={HomeScreen} />
-        </Suspense>
+        <Header />
+        <Switch>
+          <Suspense fallback={<LoadingScreen />}>
+            {/* <Route exact path='/' component={LandingPage} /> */}
+            <Route exact path='/signup' component={SignUpScreen} />
+            <Route exact path='/login' component={LoginScreen} />
+            <Route exact path='/home' component={HomeScreen} />
+          </Suspense>
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
