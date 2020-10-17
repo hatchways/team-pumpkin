45befdf1f3b7051aae1fd71b167160e7d06316dd
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-16 20:00:23 -0400

    wrap use effect the context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { MuiThemeProvider } from '@material-ui/core';
-import React, { Suspense, useContext } from 'react';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Header, LoadingScreen } from './components';
@@ -8,7 +8,16 @@ import { theme } from './themes/theme';
 import { GlobalContext } from './utils';
 
 const App = () => {
-  const appContext = useContext(GlobalContext);
+  const [appContext, setAppContext] = useState({});
+  const stateContext = useContext(GlobalContext);
+
+  useEffect(() => {
+    setAppContext(stateContext);
+
+    return () => {
+      setAppContext({});
+    };
+  }, [stateContext]);
 
   return (
     <MuiThemeProvider theme={theme}>
