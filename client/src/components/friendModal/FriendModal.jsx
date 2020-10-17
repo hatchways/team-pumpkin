0b468be174534f5a794252e9c5a175d3359249e7
TheAllen <allenlee1995@hotmail.com>
2020-10-16 23:43:38 -0600

    added friend modal

---
+++ client/src/components/friendModal/FriendModal.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Modal from '../common/Modal/Modal';
+
+const useStyles = makeStyles((theme) => {});
+
+const mockFriendList = ['Michael Jordan', 'Lebron James', 'Kobe Bryant', 'Magic Johnson'];
+
+const FriendModal = () => {
+  const classes = useStyles();
+
+  return <Modal className={className}></Modal>;
+};
+
+export default FriendModal;
