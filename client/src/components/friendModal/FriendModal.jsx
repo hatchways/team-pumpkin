1e0a1d006155c51dc873fd7f511a948b24b4fb82
TheAllen <allenlee1995@hotmail.com>
2020-10-17 00:21:08 -0600

    added a Create friend list button

--- client/src/components/friendModal/FriendModal.jsx
+++ client/src/components/friendModal/FriendModal.jsx
@@ -1,14 +1,15 @@
+import { makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
-import Modal from '../common/Modal/Modal';
+import { Modal } from '../common/Modal/Modal';
 
 const useStyles = makeStyles((theme) => {});
 
 const mockFriendList = ['Michael Jordan', 'Lebron James', 'Kobe Bryant', 'Magic Johnson'];
 
-const FriendModal = () => {
+const FriendModal = ({ open, onClose, className }) => {
   const classes = useStyles();
 
-  return <Modal className={className}></Modal>;
+  return <Modal className={className} title='Create a friend list'></Modal>;
 };
 
 export default FriendModal;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Box, makeStyles } from '@material-ui/core';
 import React, { useState } from 'react';
 import { Button, PollsModal } from '../../components';
+import FriendModal from '../../components/friendModal/FriendModal';
 import { theme } from '../../themes/theme';
 
 const useStyles = makeStyles((theme) => ({
@@ -15,14 +16,21 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
   const classes = useStyles();
   const [openPoll, setOpenPoll] = useState(false);
+  const [openFriendPoll, setFriendOpenPoll] = useState(false);
 
   const handlePollModal = () => setOpenPoll(!openPoll);
+  const handleFriendPollModal = () => setFriendOpenPoll(!openFriendPoll);
   return (
     <Box className={classes.mainContainer}>
       <Button className={classes.button} backgroundColor={theme.palette.secondary.main} onClick={handlePollModal}>
         open modal
       </Button>
+      <br></br>
+      <Button className={classes.button} backgroundColor={theme.palette.secondary.main} onClick={handleFriendPollModal}>
+        Friend Modal
+      </Button>
       <PollsModal open={openPoll} onClose={handlePollModal} />
+      <FriendModal open={openFriendPoll} onClose={handleFriendPollModal} />
     </Box>
   );
 };
