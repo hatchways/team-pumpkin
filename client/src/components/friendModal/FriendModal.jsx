032686bdc5e28f3608858f5c57d3a65f93e8ec4e
TheAllen <allenlee1995@hotmail.com>
2020-10-17 22:17:09 -0600

    added the listItems of friends

--- client/src/components/friendModal/FriendModal.jsx
+++ client/src/components/friendModal/FriendModal.jsx
@@ -1,15 +1,39 @@
-import { makeStyles } from '@material-ui/core';
+import { Box, makeStyles, List, ListItem, Grid, Divider } from '@material-ui/core';
 import React, { useState } from 'react';
+import { useValue } from '../../utils/';
 import { Modal } from '../common/Modal/Modal';
+import { InputField } from '../common/InputField/InputField';
 
-const useStyles = makeStyles((theme) => {});
+const useStyles = makeStyles((theme) => ({
+  friendModal: {
+    display: 'flex',
+    width: 500,
+  },
+}));
 
 const mockFriendList = ['Michael Jordan', 'Lebron James', 'Kobe Bryant', 'Magic Johnson'];
 
 const FriendModal = ({ open, onClose, className }) => {
   const classes = useStyles();
 
-  return <Modal className={className} title='Create a friend list'></Modal>;
+  const [friendListName, setFriendListName] = useValue('');
+  const [friends, setFriends] = useState('');
+
+  return (
+    <Modal className={className} open={open} onClose={onClose} title='Create a friend list'>
+      <Grid direction='column'>
+        <Box className={classes.friendModal}>
+          <InputField name='friendlist' placeholder='Enter name of list'></InputField>
+          {/* List of friends */}
+        </Box>
+        <List alignItems='flex-start'>
+          {mockFriendList.map((friend) => (
+            <ListItem alignItems='center'>{friend}</ListItem>
+          ))}
+        </List>
+      </Grid>
+    </Modal>
+  );
 };
 
 export default FriendModal;
