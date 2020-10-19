5dedbafc61936deae3f91861f55912f047935351
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-19 18:21:01 -0400

    add working endpoints

--- server/routes/polls/polls.js
+++ server/routes/polls/polls.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const authentication = require('../../middleware/authentication');
 const PollsMongoModel = require('../../models/Polls');
 const { cloudinary } = require('../../cloudinary/cloudinary');
-const { uuid } = require('uuidv4');
 
 router.post('/create', authentication, async (req, res) => {
   try {
-    const userId = req.user.user.id;
+    console.log('this is user', req.user);
+    const userId = req.user.id;
     const { question, friend, imagesData } = req.body;
     const uploadedResponseToCloudinaryForFirstImage = await cloudinary.uploader.upload(imagesData[0], {
       upload_preset: 'team_pumpkin',
@@ -16,7 +16,6 @@ router.post('/create', authentication, async (req, res) => {
       upload_preset: 'team_pumpkin',
     });
     const poll = {
-      pollId: uuid(),
       url1: uploadedResponseToCloudinaryForFirstImage.url,
       url2: uploadedResponseToCloudinaryForSecondImage.url,
       friend,
@@ -50,73 +49,62 @@ router.post('/create', authentication, async (req, res) => {
     const error = {
       msg: err,
     };
+    console.log(err);
     res.status(400).json(error);
   }
 });
 
 router.put('/update', authentication, async (req, res) => {
   try {
-    const userId = req.user.user.id;
+    const userId = req.user.id;
     const { question, friend, imagesData, pollId } = req.body;
-    const userPollsData = await PollsMongoModel.findOne({
-      userId,
+
+    const uploadedResponseToCloudinaryForFirstImage = await cloudinary.uploader.upload(imagesData[0], {
+      upload_preset: 'team_pumpkin',
     });
-    if (!!userPollsData) {
-      const existingUserPollIndex = userPollsData.polls.findIndex((elem) => elem.pollId === pollId);
-      const uploadedResponseToCloudinaryForFirstImage = await cloudinary.uploader.upload(imagesData[0], {
-        upload_preset: 'team_pumpkin',
-      });
-      const uploadedResponseToCloudinaryForSecondImage = await cloudinary.uploader.upload(imagesData[1], {
-        upload_preset: 'team_pumpkin',
-      });
-      userPollsData.polls[existingUserPollIndex] = {
-        pollId,
-        url1: uploadedResponseToCloudinaryForFirstImage.url,
-        url2: uploadedResponseToCloudinaryForSecondImage.url,
-        friend,
-        question,
-      };
-      const response = await PollsMongoModel.updateOne(
-        { userId },
-        {
-          $set: {
-            userId: userPollsData.userId,
-            polls: userPollsData.polls,
-          },
+    const uploadedResponseToCloudinaryForSecondImage = await cloudinary.uploader.upload(imagesData[1], {
+      upload_preset: 'team_pumpkin',
+    });
+
+    const response = await PollsMongoModel.updateOne(
+      { userId, 'polls._id': pollId },
+      {
+        $set: {
+          'polls.$.question': question,
+          'polls.$.friend': friend,
+          'polls.$.url1': uploadedResponseToCloudinaryForFirstImage.url,
+          'polls.$.url2': uploadedResponseToCloudinaryForSecondImage.url,
         },
-      );
-      res.status(200).json(response);
-    }
+      },
+    );
+    res.status(200).json(response);
   } catch (err) {
     const error = {
       msg: err,
     };
+    console.log('this is err', err);
     res.status(400).json(error);
   }
 });
 
-router.put('/delete', authentication, async (req, res) => {
+router.delete('/delete', authentication, async (req, res) => {
   try {
-    const userId = req.user.user.id;
+    const userId = req.user.id;
     const { pollId } = req.body;
-    const userPollsData = await PollsMongoModel.findOne({
-      userId,
-    });
-    if (!!userPollsData) {
-      const response = await PollsMongoModel.update(
-        { userId },
-        {
-          $pull: {
-            polls: { pollId },
-          },
+    const response = await PollsMongoModel.update(
+      { userId },
+      {
+        $pull: {
+          polls: { _id: pollId },
         },
-      );
-      res.status(200).json(response);
-    }
+      },
+    );
+    res.status(200).json(response);
   } catch (err) {
     const error = {
       msg: err,
     };
+    console.log(err);
     res.status(400).json(error);
   }
 });
