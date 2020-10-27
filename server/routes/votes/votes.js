a78898eb0e0773179acb91579d6f313d727d713c
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-27 17:29:42 -0400

    add vote endpoint

--- server/routes/votes/votes.js
+++ server/routes/votes/votes.js
@@ -4,28 +4,65 @@ const authentication = require('../../middleware/authentication');
 const PollsMongoModel = require('../../models/Polls');
 const User = require('../../models/User');
 
-router.post('/votes', authentication, async (req, res) => {
+router.post('/votes/:pollId', authentication, async (req, res) => {
   try {
     const userId = req.user.id;
-    const { pollId, voteFor, pollOwnerId } = req.body;
+    const { voteFor, pollOwnerId } = req.body;
+    const { pollId } = req.params;
     const pollOwnerDetails = await User.findOne({ _id: pollOwnerId });
     const votesArray = voteFor === 'img1' ? 'votesForUrl1' : 'votesForUrl2';
     const pollOwnerSpecificPoll = await PollsMongoModel.findOne({ userId: pollOwnerId, _id: pollId });
-    if (pollOwnerSpecificPoll[`${votesArray}`].includes(userId)) {
-      const response = await PollsMongoModel.updateOne(
-        { userId: pollOwnerId, _id: pollId },
-        { $pull: { [votesArray]: userId } },
-      );
-      res.status(200).json(response);
-    } else {
+    if (!pollOwnerSpecificPoll.votesForUrl1.includes(userId) && !pollOwnerSpecificPoll.votesForUrl2.includes(userId)) {
       const response = await PollsMongoModel.updateOne(
         { userId: pollOwnerId, _id: pollId },
         { $push: { [votesArray]: userId } },
       );
       res.status(200).json(response);
+      return;
+    }
+    if (
+      pollOwnerSpecificPoll.votesForUrl1.includes(userId) &&
+      !pollOwnerSpecificPoll.votesForUrl2.includes(userId) &&
+      voteFor === 'img2'
+    ) {
+      await PollsMongoModel.updateOne({ userId: pollOwnerId, _id: pollId }, { $pull: { votesForUrl1: userId } });
+      const response = await PollsMongoModel.updateOne(
+        { userId: pollOwnerId, _id: pollId },
+        { $push: { [votesArray]: userId } },
+      );
+      res.status(200).json(response);
+      return;
+    }
+    if (
+      !pollOwnerSpecificPoll.votesForUrl1.includes(userId) &&
+      pollOwnerSpecificPoll.votesForUrl2.includes(userId) &&
+      voteFor === 'img1'
+    ) {
+      await PollsMongoModel.updateOne({ userId: pollOwnerId, _id: pollId }, { $pull: { votesForUrl2: userId } });
+      const response = await PollsMongoModel.updateOne(
+        { userId: pollOwnerId, _id: pollId },
+        { $push: { [votesArray]: userId } },
+      );
+      res.status(200).json(response);
+      return;
+    }
+    if (
+      (pollOwnerSpecificPoll.votesForUrl1.includes(userId) &&
+        !pollOwnerSpecificPoll.votesForUrl2.includes(userId) &&
+        voteFor === 'img1') ||
+      (!pollOwnerSpecificPoll.votesForUrl1.includes(userId) &&
+        pollOwnerSpecificPoll.votesForUrl2.includes(userId) &&
+        voteFor === 'img2')
+    ) {
+      const response = await PollsMongoModel.updateOne(
+        { userId: pollOwnerId, _id: pollId },
+        { $pull: { [votesArray]: userId } },
+      );
+      res.status(200).json(response);
+      return;
     }
   } catch (err) {
-    console.log('this is errr', err);
+    res.status(400).json(err);
   }
 });
 
