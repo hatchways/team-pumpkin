bb2486ea2b97ec74e1e5498be443cd8cca246b8c
Zeeshan <sk.zeeshan1992@gmail.com>
2020-10-26 21:43:54 -0400

    add votes endpoint

--- server/routes/votes/votes.js
+++ server/routes/votes/votes.js
@@ -8,12 +8,25 @@ router.post('/votes', authentication, async (req, res) => {
   try {
     const userId = req.user.id;
     const { pollId, voteFor, pollOwnerId } = req.body;
-    console.log('this is re body', req.body);
     const pollOwnerDetails = await User.findOne({ _id: pollOwnerId });
-    console.log('PollOwner', pollOwnerDetails);
-    const response = await PollsMongoModel.findOne({ userId: pollOwnerId, _id: pollId });
-    res.status(200).json(response);
-  } catch (err) {}
+    const votesArray = voteFor === 'img1' ? 'votesForUrl1' : 'votesForUrl2';
+    const pollOwnerSpecificPoll = await PollsMongoModel.findOne({ userId: pollOwnerId, _id: pollId });
+    if (pollOwnerSpecificPoll[`${votesArray}`].includes(userId)) {
+      const response = await PollsMongoModel.updateOne(
+        { userId: pollOwnerId, _id: pollId },
+        { $pull: { [votesArray]: userId } },
+      );
+      res.status(200).json(response);
+    } else {
+      const response = await PollsMongoModel.updateOne(
+        { userId: pollOwnerId, _id: pollId },
+        { $push: { [votesArray]: userId } },
+      );
+      res.status(200).json(response);
+    }
+  } catch (err) {
+    console.log('this is errr', err);
+  }
 });
 
 module.exports = router;
