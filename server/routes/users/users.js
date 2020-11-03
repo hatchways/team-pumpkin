a3a78a340cee6977e2f1a8c603fc7ebde91e6075
Zeeshan <sk.zeeshan1992@gmail.com>
2020-11-02 21:57:20 -0500

    add endpoint for fething multiple user data

--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -8,33 +8,37 @@ router.get('/users', authentication, async (req, res) => {
   try {
     const userId = req.user.id;
     const { votesForUrl1, votesForUrl2 } = req.body;
-    console.log('this is body', req.body);
-    let finalList = [];
-    votesForUrl1.map(async (element) => {
-      const result = await User.findOne({ _id: element });
-      const userData = {
-        userName: result.name,
-        imgUrl:
-          'https://img1.looper.com/img/gallery/things-about-thanos-that-didnt-make-it-into-the-mcu/intro-1590779038.jpg',
-        votesFor: 'img1',
-      };
-      console.log('this is userData', userData);
-      finalList = finalList.concat(userData);
+    const userListForVote1 = await User.find({
+      _id: {
+        $in: votesForUrl1,
+      },
     });
-    console.log('this is first first list', finalList);
-    votesForUrl2.map(async (element) => {
-      const result = await User.findOne({ _id: element });
-      const userData = {
-        userName: result.name,
-        imgUrl:
-          'https://img1.looper.com/img/gallery/things-about-thanos-that-didnt-make-it-into-the-mcu/intro-1590779038.jpg',
-        votesFor: 'img1',
-      };
-      finalList.push(userData);
+    const userListForVote2 = await User.find({
+      _id: {
+        $in: votesForUrl2,
+      },
     });
-    console.log('this is first first list', finalList);
-    res.status(200).json(finalList);
+    let result = [];
+    const dupUserListVote1 = [...userListForVote1];
+    const dupUserListVote2 = [...userListForVote2];
+    dupUserListVote1.map((elem) =>
+      result.push({
+        name: elem.name,
+        avatar: null,
+        votesFor: 'img1',
+      }),
+    );
+    dupUserListVote1.map((elem) => (elem.votesFor = 'img1'));
+    dupUserListVote2.map((elem) =>
+      result.push({
+        name: elem.name,
+        avatar: null,
+        votesFor: 'img2',
+      }),
+    );
+    res.status(200).json(result);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
